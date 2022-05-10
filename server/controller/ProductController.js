e35494de47b36d26bfa9d762dd960e70722054f7
doxh23 <adrien.peters95@gmail.com>
2022-05-10 19:02:18 +0200

    use regex expression for numericfilter for product

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -19,17 +19,17 @@ const createProduct = async (req, res) => {
  * @param {respond send from server} res
  */
 const updateProduct = catchAsyncError(async (req, res, next) => {
-    let product = await products.findById(req.params.id);
-    if (!product) {
-      return next(new ErrorHandler("product not found", 404));
-    }
+  let product = await products.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("product not found", 404));
+  }
 
-    product = await products.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-      useFindAndModify: false,
-    });
-    res.status(200).json({ succes:true, product });
+  product = await products.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({ succes: true, product });
 });
 /**
  * delete a product
@@ -37,16 +37,16 @@ const updateProduct = catchAsyncError(async (req, res, next) => {
  * @param {respond send from server} res
  */
 const deleteProduct = catchAsyncError(async (req, res) => {
-    let product = await products.findById(req.params.id);
-    if (!product) {
-      return next(new ErrorHandler("product not found", 404));
-    }
-    product.remove();
-    res.status(200).json({
-      success: true,
-      message: "Product delete success",
-    });
+  let product = await products.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("product not found", 404));
+  }
+  product.remove();
+  res.status(200).json({
+    success: true,
+    message: "Product delete success",
   });
+});
 
 //---------------------------------------------ALL USER CONTROLLER ------------------------------------------------
 /**
@@ -55,22 +55,53 @@ const deleteProduct = catchAsyncError(async (req, res) => {
  * @param {respond send from server} res
  */
 const getAllProduct = catchAsyncError(async (req, res) => {
-    let product = await products.find({});
-    res.status(200).json({ product });
-}
-);
+  /**
+   * afficher les elements par rapport aux @param {noms}
+   * afficher les elements par rapport aux @param {noms}
+   * afficher les elements par rapport aux @param {prix} plus grand -plus petit-etc
+   * trier par rapport a un parametre @param {sort} nom description prix / etc
+   * affichier juste certain element @param {select}
+   *
+   */
+  let { name, category, sort, select, numericFilter } = req.query;
+  let objectQuery = {};
+  if (name) {
+    objectQuery.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilter) {
+    const OperatorMap = {
+      "<": "$lt",
+      ">": "$gt",
+      "<=": "$lte",
+      ">=": "$gte",
+      "=": "$eq",
+    };
+    const regexExp = /\b([<>]=?|=)\b/g
+    // price> 30
+    //  {price : {$gte : 30}}
+   numericFilter= numericFilter.replace(regexExp,(match) => `-${OperatorMap[match]}-`)
+console.log(numericFilter)
+  numericFilter.split(',').forEach(el => {
+    let [field,operator,value]= el.split('-')
+     objectQuery[field]= {[operator]:value}
+  });
+    
+    console.log(objectQuery)
+  }
+  let product = await products.find(objectQuery);
+  res.status(200).json({ product });
+});
 /**
  * get one product  by id
  * @param {request from server} req
  * @param {respond send from server} res
  */
-const getOneProduct = catchAsyncError(async (req, res,next) => {
-    let product = await products.findById(req.params.id);
-    if (!product){
-      return next(new ErrorHandler("product not found",404))
-    }
-    res.status(200).json({ succes: true,product });
-
+const getOneProduct = catchAsyncError(async (req, res, next) => {
+  let product = await products.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("product not found", 404));
+  }
+  res.status(200).json({ succes: true, product });
 });
 
 module.exports = {
